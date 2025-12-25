410a47719e7047faf05f08ae3c83a38c18abc8ca
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2025-12-25 16:07:12 +0500

    fix layout issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import { Footer } from "@/components/Footer";
 import { About } from "@/pages/About";
 import { ProjectDetail } from "@/pages/ProjectDetail";
 import { Projects } from "@/pages/Projects";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <ScrollToTop />
 
         <Navbar />
 
